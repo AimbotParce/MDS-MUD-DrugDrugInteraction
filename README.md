ed6565c679c2604769671f88be9dabe6ef28a978
eZWALT <waltertheshadow333@gmail.com>
2025-05-06 17:55:13 +0200

    Added initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MDS-MUD-DrugDrugInteraction
+
+## Description
 ML and NN pipelines for detecting interactions between drugs in text.
+
+## Usage 
+
+To execute the ML-DDI script:
+
+```sh 
+cd ml 
+chmod +x run.sh 
+./run.sh
+```
+
+To execute the DL-DDI script:
+
+```sh
+cd dl
+chmod +x run.sh 
+./run.sh
+```
---
+++ ml/corenlp-server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Runs Stanford CoreNLP server
+
+# set this path to the directory where you decompressed StanfordCore
+
+# Dani posa el teu custom path :)
+STANFORDDIR=~/Desktop/College/stanford-corenlp-4.5.9
+
+if [ -f /tmp/corenlp-server.running ]; then
+    echo "server already running"
+else
+    echo java -mx5g -cp \"$STANFORDDIR/*\" edu.stanford.nlp.pipeline.StanfordCoreNLPServer $*
+    java -mx5g -cp "$STANFORDDIR/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer $* &
+    echo $! > /tmp/corenlp-server.running
+    wait
+    rm /tmp/corenlp-server.running
+fi
