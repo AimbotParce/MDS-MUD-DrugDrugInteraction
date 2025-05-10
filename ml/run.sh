73261404c811c89552591d22792add62b598d3e8
AimbotParce <marcparcerisa@gmail.com>
2025-05-10 11:14:42 +0200

    Remove old random and change directories

--- ml/run.sh
+++ ml/run.sh
@@ -2,18 +2,22 @@
 
 BASEDIR=..
 
+mkdir -p $BASEDIR/data/cache
+mkdir -p $BASEDIR/data/models
+mkdir -p $BASEDIR/data/out
+
 # extract features
 echo "Extracting features"
-python3 extract-features.py $BASEDIR/data/devel/ > devel.cod &
-python3 extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
+python3 extract-features.py $BASEDIR/data/devel/ > $BASEDIR/data/cache/devel.cod &
+python3 extract-features.py $BASEDIR/data/train/ | tee $BASEDIR/data/cache/train.cod | cut -f4- > $BASEDIR/data/cache/train.cod.cl
 
 # train model
 echo "Training model"
-python3 train-sklearn.py model.joblib vectorizer.joblib < train.cod.cl
+python3 train-sklearn.py $BASEDIR/data/models/model.joblib $BASEDIR/data/models/vectorizer.joblib < $BASEDIR/data/cache/train.cod.cl
 # run model
 echo "Running model..."
-python3 predict-sklearn.py model.joblib vectorizer.joblib < devel.cod > devel.out
+python3 predict-sklearn.py $BASEDIR/data/models/model.joblib $BASEDIR/data/models/vectorizer.joblib < $BASEDIR/data/cache/devel.cod > $BASEDIR/data/out/devel.out
 # evaluate results
 echo "Evaluating results..."
-python3 evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel.stats
+python3 evaluator.py DDI $BASEDIR/data/devel/ $BASEDIR/data/out/devel.out > $BASEDIR/data/out/devel.stats
 
