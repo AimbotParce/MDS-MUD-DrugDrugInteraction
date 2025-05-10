7a793451bd5561bd0ba2fa332cea05dad361840c
AimbotParce <marcparcerisa@gmail.com>
2025-05-10 11:28:03 +0200

    Add type hinting to extract-features

--- ml/extract-features.py
+++ ml/extract-features.py
@@ -2,65 +2,68 @@
 
 import sys
 from os import listdir
-
+from typing import Dict, TypedDict
 from xml.dom.minidom import parse
 
-from deptree import *
-#import patterns
+from deptree import deptree
 
+# import patterns
 
-## ------------------- 
+OffsetDict = TypedDict(start=int, end=int)
+
+## -------------------
 ## -- Convert a pair of drugs and their context in a feature vector
 
-def extract_features(tree, entities, e1, e2) :
-   feats = set()
 
-   # get head token for each gold entity
-   tkE1 = tree.get_fragment_head(entities[e1]['start'],entities[e1]['end'])
-   tkE2 = tree.get_fragment_head(entities[e2]['start'],entities[e2]['end'])
+def extract_features(dependency_tree: deptree, entities: Dict[str, OffsetDict], entity_1: str, entity_2: str):
+    feats: Dict[str, str] = {}
 
-   if tkE1 is not None and tkE2 is not None:
-      # features for tokens in between E1 and E2
-      #for tk in range(tkE1+1, tkE2) :
-      tk=tkE1+1
-      try:
-        while (tree.is_stopword(tk)):
-          tk += 1
-      except:
-        return set()
-      word  = tree.get_word(tk)
-      lemma = tree.get_lemma(tk).lower()
-      tag = tree.get_tag(tk)
-      feats.add("lib=" + lemma)
-      feats.add("wib=" + word)
-      feats.add("lpib=" + lemma + "_" + tag)
-      
-      eib = False
-      for tk in range(tkE1+1, tkE2) :
-         if tree.is_entity(tk, entities):
-            eib = True 
-      
-	  # feature indicating the presence of an entity in between E1 and E2
-      feats.add('eib='+ str(eib))
+    # get head token for each gold entity
+    tkE1 = dependency_tree.get_fragment_head(entities[entity_1]["start"], entities[entity_1]["end"])
+    tkE2 = dependency_tree.get_fragment_head(entities[entity_2]["start"], entities[entity_2]["end"])
 
-      # features about paths in the tree
-      lcs = tree.get_LCS(tkE1,tkE2)
-      
-      path1 = tree.get_up_path(tkE1,lcs)
-      path1 = "<".join([tree.get_lemma(x)+"_"+tree.get_rel(x) for x in path1])
-      feats.add("path1="+path1)
+    if tkE1 is not None and tkE2 is not None:
+        # features for tokens in between E1 and E2
+        # for tk in range(tkE1+1, tkE2) :
+        tk = tkE1 + 1
+        try:
+            while dependency_tree.is_stopword(tk):
+                tk += 1
+        except:
+            return {}
+        word = dependency_tree.get_word(tk)
+        lemma = dependency_tree.get_lemma(tk).lower()
+        tag = dependency_tree.get_tag(tk)
+        feats.add("lib=" + lemma)
+        feats.add("wib=" + word)
+        feats.add("lpib=" + lemma + "_" + tag)
 
-      path2 = tree.get_down_path(lcs,tkE2)
-      path2 = ">".join([tree.get_lemma(x)+"_"+tree.get_rel(x) for x in path2])
-      feats.add("path2="+path2)
+        eib = False
+        for tk in range(tkE1 + 1, tkE2):
+            if dependency_tree.is_entity(tk, entities):
+                eib = True
 
-      path = path1+"<"+tree.get_lemma(lcs)+"_"+tree.get_rel(lcs)+">"+path2      
-      feats.add("path="+path)
-      
-   return feats
+        # feature indicating the presence of an entity in between E1 and E2
+        feats.add("eib=" + str(eib))
+
+        # features about paths in the tree
+        lcs = dependency_tree.get_LCS(tkE1, tkE2)
+
+        path1 = dependency_tree.get_up_path(tkE1, lcs)
+        path1 = "<".join([dependency_tree.get_lemma(x) + "_" + dependency_tree.get_rel(x) for x in path1])
+        feats.add("path1=" + path1)
+
+        path2 = dependency_tree.get_down_path(lcs, tkE2)
+        path2 = ">".join([dependency_tree.get_lemma(x) + "_" + dependency_tree.get_rel(x) for x in path2])
+        feats.add("path2=" + path2)
+
+        path = path1 + "<" + dependency_tree.get_lemma(lcs) + "_" + dependency_tree.get_rel(lcs) + ">" + path2
+        feats.add("path=" + path)
+
+    return feats
 
 
-## --------- MAIN PROGRAM ----------- 
+## --------- MAIN PROGRAM -----------
 ## --
 ## -- Usage:  extract_features targetdir
 ## --
@@ -71,44 +74,53 @@ def extract_features(tree, entities, e1, e2) :
 datadir = sys.argv[1]
 
 # process each file in directory
-for f in listdir(datadir) :
+for f in listdir(datadir):
 
     # parse XML file, obtaining a DOM tree
-    tree = parse(datadir+"/"+f)
+    xml_tree = parse(datadir + "/" + f)
 
     # process each sentence in the file
-    sentences = tree.getElementsByTagName("sentence")
-    for s in sentences :
-        sid = s.attributes["id"].value   # get sentence id
-        stext = s.attributes["text"].value   # get sentence text
+    xml_sentences = xml_tree.getElementsByTagName("sentence")
+    for xml_sentence in xml_sentences:
+        sentence_id = xml_sentence.attributes["id"].value  # get sentence id
+        sentence_text = xml_sentence.attributes["text"].value  # get sentence text
         # load sentence entities
-        entities = {}
-        ents = s.getElementsByTagName("entity")
-        for e in ents :
-           id = e.attributes["id"].value
-           offs = e.attributes["charOffset"].value.split("-")           
-           entities[id] = {'start': int(offs[0]), 'end': int(offs[-1])}
+        entities: Dict[str, OffsetDict] = {}
+        xml_entities = xml_sentence.getElementsByTagName("entity")
+        for xml_entity in xml_entities:
+            entity_id = xml_entity.attributes["id"].value
+            entity_offsets = xml_entity.attributes["charOffset"].value.split("-")
+            entities[entity_id] = {"start": int(entity_offsets[0]), "end": int(entity_offsets[-1])}
 
         # there are no entity pairs, skip sentence
-        if len(entities) <= 1 : continue
+        if len(entities) <= 1:
+            continue
 
         # analyze sentence
-        analysis = deptree(stext)
+        dependency_tree = deptree(sentence_text)
 
         # for each pair in the sentence, decide whether it is DDI and its type
-        pairs = s.getElementsByTagName("pair")
-        for p in pairs:
+        xml_pairs = xml_sentence.getElementsByTagName("pair")
+        for xml_pair in xml_pairs:
             # ground truth
-            ddi = p.attributes["ddi"].value
-            if (ddi=="true") : dditype = p.attributes["type"].value
-            else : dditype = "null"
+            is_interaction = xml_pair.attributes["ddi"].value
+            if is_interaction == "true":
+                interaction_type = xml_pair.attributes["type"].value
+            else:
+                interaction_type = "null"
             # target entities
-            id_e1 = p.attributes["e1"].value
-            id_e2 = p.attributes["e2"].value
+            interaction_entity_1 = xml_pair.attributes["e1"].value
+            interaction_entity_2 = xml_pair.attributes["e2"].value
             # feature extraction
 
-            feats = extract_features(analysis,entities,id_e1,id_e2) 
+            features = extract_features(dependency_tree, entities, interaction_entity_1, interaction_entity_2)
             # resulting vector
-            if len(feats) != 0:
-              print(sid, id_e1, id_e2, dditype, "\t".join(feats), sep="\t")
-
+            if len(features) != 0:
+                print(
+                    sentence_id,
+                    interaction_entity_1,
+                    interaction_entity_2,
+                    interaction_type,
+                    "\t".join(f"{k}={v}" for k, v in features.items()),
+                    sep="\t",
+                )
