56a0f379e79c3274e82b4362c1e3b67072537fe1
AimbotParce <marcparcerisa@gmail.com>
2025-05-12 22:24:37 +0200

    Add evaluator to dl, format the training code, and correct the run scripts for our current structure

--- dl/predict.py
+++ dl/predict.py
@@ -3,31 +3,30 @@
 import sys
 from os import system
 
-from tensorflow.keras.models import Model,load_model
-
-from dataset import *
 from codemaps import *
-import evaluator
+from dataset import *
+from keras.models import Model, load_model
 
-## --------- Entity extractor ----------- 
+## --------- Entity extractor -----------
 ## -- Extract drug entities from given text and return them as
 ## -- a list of dictionaries with keys "offset", "text", and "type"
 
-def output_interactions(data, preds, outfile) :
 
-   #print(testdata[0])
-   outf = open(outfile, 'w')
-   for exmp,tag in zip(data.sentences(),preds) :
-      sid = exmp['sid']
-      e1 = exmp['e1']
-      e2 = exmp['e2']
-      if tag!='null' :
-         print(sid, e1, e2, tag, sep="|", file=outf)
-            
-   outf.close()
+def output_interactions(data, preds, outfile):
 
-   
-## --------- MAIN PROGRAM ----------- 
+    # print(testdata[0])
+    outf = open(outfile, "w")
+    for exmp, tag in zip(data.sentences(), preds):
+        sid = exmp["sid"]
+        e1 = exmp["e1"]
+        e2 = exmp["e2"]
+        if tag != "null":
+            print(sid, e1, e2, tag, sep="|", file=outf)
+
+    outf.close()
+
+
+## --------- MAIN PROGRAM -----------
 ## --
 ## -- Usage:  baseline-NER.py target-dir
 ## --
@@ -49,6 +48,3 @@ Y = [codes.idx2label(np.argmax(s)) for s in Y]
 
 # extract relations
 output_interactions(testdata, Y, outfile)
-
-
-
--- ml/run.sh
+++ ml/run.sh
@@ -1,23 +1,27 @@
 #! /bin/bash
 
 BASEDIR=..
+DATA=$BASEDIR/data
+CACHE=$DATA/cache
+MODELS=$DATA/models
+OUT=$DATA/out
 
-mkdir -p $BASEDIR/data/cache
-mkdir -p $BASEDIR/data/models
-mkdir -p $BASEDIR/data/out
+mkdir -p $CACHE
+mkdir -p $MODELS
+mkdir -p $OUT
 
 # extract features
 echo "Extracting features"
-python3 extract-features.py $BASEDIR/data/devel/ > $BASEDIR/data/cache/devel.cod &
-python3 extract-features.py $BASEDIR/data/train/ | tee $BASEDIR/data/cache/train.cod | cut -f4- > $BASEDIR/data/cache/train.cod.cl
+python3 extract-features.py $DATA/devel/ > $CACHE/devel.cod &
+python3 extract-features.py $DATA/train/ | tee $CACHE/train.cod | cut -f4- > $CACHE/train.cod.cl
 
 # train model
 echo "Training model"
-python3 train-sklearn.py $BASEDIR/data/models/model.joblib $BASEDIR/data/models/vectorizer.joblib < $BASEDIR/data/cache/train.cod.cl
+python3 train-sklearn.py $MODELS/model.joblib $MODELS/vectorizer.joblib < $CACHE/train.cod.cl
 # run model
 echo "Running model..."
-python3 predict-sklearn.py $BASEDIR/data/models/model.joblib $BASEDIR/data/models/vectorizer.joblib < $BASEDIR/data/cache/devel.cod > $BASEDIR/data/out/devel.out
+python3 predict-sklearn.py $MODELS/model.joblib $MODELS/vectorizer.joblib < $CACHE/devel.cod > $OUT/devel.out
 # evaluate results
 echo "Evaluating results..."
-python3 evaluator.py DDI $BASEDIR/data/devel/ $BASEDIR/data/out/devel.out > $BASEDIR/data/out/devel.stats
+python3 evaluator.py DDI $DATA/devel/ $OUT/devel.out > $OUT/devel.stats
 
