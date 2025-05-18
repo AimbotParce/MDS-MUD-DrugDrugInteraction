538d31dd667da9397f91f085fae6efd44d5ba225
Dani W. Falco <daniwero2003@gmail.com>
2025-05-18 12:18:42 +0200

    try once again

--- dl/codemaps.py
+++ dl/codemaps.py
@@ -1,63 +1,51 @@
-from typing import Dict, Union, List
-
+from typing import Dict, Union, List, Type  # Import Type
 import numpy as np
-from dataset import Dataset  #
-
-# from keras.preprocessing.sequence import pad_sequences # Not needed for BERT tokenization like this
-from tensorflow.keras.utils import to_categorical  # Assuming tf.keras
-from transformers import BertTokenizer
+from dataset import Dataset
+from tensorflow.keras.utils import to_categorical
+from transformers import BertTokenizer, BertTokenizerFast  # Add BertTokenizerFast
 
 
 class Codemaps:
-    """
-    Class to manage BERT tokenization for input sentences and encode labels.
-    """
-
     def __init__(
         self,
         data: Union[Dataset, str],
         maxlen: int = None,
         bert_model_name: str = "Linglab/PharmBERT-uncased",
-    ):
-        """
-        Constructor for the Codemaps class.
-        If data is a Dataset object, it initializes the tokenizer and label_index.
-        If data is a string (path to saved codemaps), it loads them.
-
-        Args:
-            data (Dataset | str): Either a Dataset object or a path to load codemaps.
-            maxlen (int, optional): The maximum length of the tokenized sentences.
-                                    Required if data is a Dataset object.
-            bert_model_name (str): Name of the pre-trained BERT model for the tokenizer.
-        """
-
+        tokenizer_class: Type[
+            Union[BertTokenizer, BertTokenizerFast]
+        ] = BertTokenizerFast,
+    ):  # Allow specifying tokenizer
         self.maxlen: int
         self.label_index: Dict[str, int]
-        self.tokenizer: BertTokenizer
+        self.tokenizer: Union[BertTokenizer, BertTokenizerFast]  # Type hint
         self.bert_model_name: str = bert_model_name
+        self._tokenizer_class = tokenizer_class  # Store tokenizer class
+
+        # Define special tokens to be added
+        self.special_tokens_list = ["<DRUG1>", "<DRUG2>", "<DRUG_OTHER>"]
 
         if isinstance(data, Dataset) and maxlen is not None:
             self.maxlen = maxlen
-            self.tokenizer = BertTokenizer.from_pretrained(self.bert_model_name)
-            # Add special drug tokens if they are not already part of the tokenizer's vocabulary
-            # These tokens are used in your dataset.py file
-            special_tokens_dict = {
-                "additional_special_tokens": ["<DRUG1>", "<DRUG2>", "<DRUG_OTHER>"]
-            }
-            self.tokenizer.add_special_tokens(special_tokens_dict)
-
+            self.tokenizer = self._tokenizer_class.from_pretrained(self.bert_model_name)
+            self.tokenizer.add_special_tokens(
+                {"additional_special_tokens": self.special_tokens_list}
+            )
             self._create_label_index(data)
 
         elif isinstance(data, str) and maxlen is None:  # Loading from file
             self._load_indexes(data)
-            # Initialize tokenizer after loading, bert_model_name should be saved or passed
-            # For simplicity, we'll re-initialize it here. Ideally, save/load tokenizer info.
-            self.tokenizer = BertTokenizer.from_pretrained(self.bert_model_name)
-            special_tokens_dict = {
-                "additional_special_tokens": ["<DRUG1>", "<DRUG2>", "<DRUG_OTHER>"]
-            }
-            self.tokenizer.add_special_tokens(special_tokens_dict)
-
+            # Re-initialize tokenizer after loading
+            self.tokenizer = self._tokenizer_class.from_pretrained(self.bert_model_name)
+            # Important: ensure special tokens are re-added if not part of base vocab
+            # or if tokenizer doesn't save this info with from_pretrained.
+            # add_special_tokens usually returns the number of tokens added.
+            num_added_toks = self.tokenizer.add_special_tokens(
+                {"additional_special_tokens": self.special_tokens_list}
+            )
+            if num_added_toks > 0:
+                print(
+                    f"Re-added {num_added_toks} special tokens to tokenizer in Codemaps during load."
+                )
         else:
             print("codemaps: Invalid or missing parameters in constructor")
             exit(1)
@@ -69,15 +57,11 @@ class Codemaps:
         self.label_index = {t: i for i, t in enumerate(sorted(list(labels)))}
 
     def _load_indexes(self, name: str):
-        """
-        Load maxlen and label_index from file.
-        Tokenizer is re-initialized based on self.bert_model_name.
-        """
         self.maxlen = 0
         self.label_index = {}
-
-        # Try to load bert_model_name if saved, otherwise use default
         temp_bert_model_name = self.bert_model_name
+        # Default tokenizer class if not saved (for backward compatibility if you add it later)
+        temp_tokenizer_class_name = self._tokenizer_class.__name__
 
         with open(name) as f:
             for line in f.readlines():
@@ -94,55 +78,56 @@ class Codemaps:
                     self.label_index[k] = int(parts[2])
                 elif t == "BERT_MODEL_NAME":
                     temp_bert_model_name = k
+                elif t == "TOKENIZER_CLASS":  # Save tokenizer class name
+                    temp_tokenizer_class_name = k
 
         self.bert_model_name = temp_bert_model_name
+        if temp_tokenizer_class_name == "BertTokenizerFast":
+            self._tokenizer_class = BertTokenizerFast
+        else:  # Default or specific handling
+            self._tokenizer_class = BertTokenizer
 
     def save(self, name: str):
-        # save maxlen and label_index
         with open(name, "w") as f:
             print("MAXLEN", self.maxlen, "-", file=f)
+            print("BERT_MODEL_NAME", self.bert_model_name, "-", file=f)
             print(
-                "BERT_MODEL_NAME", self.bert_model_name, "-", file=f
-            )  # Save model name
+                "TOKENIZER_CLASS", self._tokenizer_class.__name__, "-", file=f
+            )  # Save tokenizer class name
             for key in self.label_index:
                 print("LABEL", key, self.label_index[key], file=f)
 
     def encode_texts(self, data: Dataset):
-        """
-        Encode sentences from data using the BERT tokenizer.
-        Returns a dictionary of input_ids and attention_mask.
-        """
         sentences = [
             " ".join([token["form"] for token in s["sent"]]) for s in data.sentences()
         ]
-
         encoded_inputs = self.tokenizer.batch_encode_plus(
             sentences,
             add_special_tokens=True,
             max_length=self.maxlen,
-            padding="max_length",  # or True
+            padding="max_length",
             truncation=True,
             return_attention_mask=True,
-            return_tensors="tf",  # Return TensorFlow tensors
+            return_tensors="tf",
         )
         return {
             "input_ids": encoded_inputs["input_ids"],
             "attention_mask": encoded_inputs["attention_mask"],
         }
 
-    def encode_labels(self, data: Dataset):  #
-        Y = [self.label_index[s["type"]] for s in data.sentences()]  #
-        Y = [to_categorical(i, num_classes=self.get_n_labels()) for i in Y]  #
-        return np.array(Y)  #
+    def encode_labels(self, data: Dataset):
+        Y = [self.label_index[s["type"]] for s in data.sentences()]
+        Y = [to_categorical(i, num_classes=self.get_n_labels()) for i in Y]
+        return np.array(Y)
 
-    def get_n_labels(self):  #
-        return len(self.label_index)  #
+    def get_n_labels(self):
+        return len(self.label_index)
 
-    def label2idx(self, l: str):  #
-        return self.label_index[l]  #
+    def label2idx(self, l: str):
+        return self.label_index[l]
 
-    def idx2label(self, i: int):  #
-        for l_key, l_val in self.label_index.items():  #
-            if l_val == i:  #
-                return l_key  #
+    def idx2label(self, i: int):
+        for l_key, l_val in self.label_index.items():
+            if l_val == i:
+                return l_key
         raise KeyError(f"Index {i} not found in label_index")
--- dl/train.py
+++ dl/train.py
@@ -2,84 +2,98 @@
 import sys
 from contextlib import redirect_stdout
 
-import numpy as np  # Added for predict if you use argmax there
-from codemaps import Codemaps  #
-from dataset import Dataset  #
+import numpy as np
+import tensorflow as tf  # Explicitly import tensorflow
 from tensorflow.keras.layers import (
     Input,
     Dense,
     Dropout,
     Flatten,
     Conv1D,
-)  # Assuming tf.keras
+    Layer,
+)  # Import Layer
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
-from tensorflow.keras.optimizers import Adam  # Recommended for BERT fine-tuning
+from tensorflow.keras.optimizers import Adam
 
-from transformers import TFBertModel
+from transformers import (
+    TFBertModel,
+    BertTokenizerFast,
+)  # Using BertTokenizerFast like in example
+
+from codemaps import Codemaps
+from dataset import Dataset
 
 BERT_MODEL_NAME = "Linglab/PharmBERT-uncased"
 
 
-def build_network(
-    codes: Codemaps, learning_rate=5e-5
-):  # Pass codes object for maxlen, n_labels
-    """
-    Builds the network with a BERT base.
-    """
-    n_labels = codes.get_n_labels()  #
-    max_len = codes.maxlen  #
+# Custom Keras Layer to wrap TFBertModel
+class BertEmbeddingLayer(Layer):
+    def __init__(self, bert_model_name, **kwargs):
+        super(BertEmbeddingLayer, self).__init__(**kwargs)
+        self.bert_model = TFBertModel.from_pretrained(bert_model_name, from_pt=True)
+        self.bert_model.trainable = True  # Make BERT layers trainable
 
-    # BERT input layers
+    def call(self, inputs):
+        # Inputs should be a list or tuple: [input_ids, attention_mask]
+        input_ids, attention_mask = inputs
+        outputs = self.bert_model(input_ids=input_ids, attention_mask=attention_mask)
+        return outputs.last_hidden_state  # Or outputs[0]
+
+    def get_config(self):  # For saving and loading the model
+        config = super().get_config()
+        # Note: bert_model itself is not easily serializable directly into config.
+        # We rely on from_pretrained during loading.
+        # For simplicity, we're not adding bert_model_name to config,
+        # as it's a global or passed during __init__.
+        # If you need to save/load this custom layer with varying bert_model_name,
+        # you'd store it in config and use it in from_config.
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        # A more robust from_config would re-initialize self.bert_model
+        # using a bert_model_name stored in the config.
+        # For now, this relies on the __init__ to get the BERT_MODEL_NAME.
+        # This might require careful handling if BERT_MODEL_NAME changes.
+        # A simple workaround for loading is to register the custom layer
+        # and potentially re-initialize with the correct model name.
+        # Or, pass bert_model_name in config and use it here.
+        # For now, we'll keep it simple as the `build_network` re-creates it.
+        return cls(
+            bert_model_name=BERT_MODEL_NAME, **config
+        )  # HACK: Assumes BERT_MODEL_NAME is accessible
+
+
+def build_network(codes: Codemaps, learning_rate=5e-5):
+    n_labels = codes.get_n_labels()
+    max_len = codes.maxlen
+
     input_ids = Input(shape=(max_len,), dtype="int32", name="input_ids")
     attention_mask = Input(shape=(max_len,), dtype="int32", name="attention_mask")
 
-    # BERT model
-    bert_model = TFBertModel.from_pretrained(BERT_MODEL_NAME, name="bert_model")
-    # Make BERT layers trainable for fine-tuning
-    bert_model.trainable = True
+    # Use the custom BertEmbeddingLayer
+    bert_embedding_layer = BertEmbeddingLayer(
+        bert_model_name=BERT_MODEL_NAME, name="bert_embedding"
+    )
+    bert_output = bert_embedding_layer([input_ids, attention_mask])
 
-    # Get the last hidden state from BERT
-    # The output is a tuple, [0] is last_hidden_state, [1] is pooler_output
-    bert_output = bert_model(input_ids, attention_mask=attention_mask)[
-        0
-    ]  # Using last_hidden_state
-
-    # Your existing CNN layers
-    # You might need to adjust filter sizes or architecture if BERT's output dimension is different
-    # BERT base uncased hidden size is 768. Your previous embedding dim was 100.
-    # Using last_hidden_state (batch_size, max_len, hidden_size=768)
-
-    # Option 1: Use CLS token output (representation of the whole sequence)
-    # cls_token_output = bert_output[:, 0, :] # Shape: (batch_size, hidden_size)
-    # x = Dropout(0.5)(cls_token_output)
-    # x = Dense(128, activation='relu', kernel_regularizer=l2(0.01))(x) # Example intermediate dense layer
-    # x = Dropout(0.3)(x)
-    # out = Dense(n_labels, activation="softmax", kernel_regularizer=l2(0.01))(x)
-
-    # Option 2: Keep CNN layers on top of BERT's sequence output (last_hidden_state)
-    # This is more similar to your original architecture's spirit.
-    x = bert_output  # (batch_size, max_len, 768)
+    x = bert_output
 
     x = Conv1D(
         filters=128, kernel_size=5, strides=1, activation="relu", padding="same"
-    )(
-        x
-    )  # Increased filters
+    )(x)
     x = Dropout(0.5)(x)
     x = Conv1D(filters=64, kernel_size=5, strides=1, activation="relu", padding="same")(
         x
-    )  # Increased filters
+    )
     x = Dropout(0.2)(x)
-    # x = Conv1D(filters=32, kernel_size=5, strides=1, activation="relu", padding="same")(x) # Potentially adjust
-    # x = Dropout(0.2)(x)
-    x = Flatten()(x)  #
-    x = Dropout(0.5)(x)  #
-    out = Dense(n_labels, activation="softmax", kernel_regularizer=l2(0.01))(x)  #
+    x = Flatten()(x)
+    x = Dropout(0.5)(x)
+    out = Dense(n_labels, activation="softmax", kernel_regularizer=l2(0.01))(x)
 
     model = Model(inputs=[input_ids, attention_mask], outputs=out)
 
-    # Use Adam optimizer with a learning rate suitable for BERT fine-tuning
     optimizer = Adam(learning_rate=learning_rate)
     model.compile(
         loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"]
@@ -96,47 +110,89 @@ if __name__ == "__main__":
         )
         sys.exit(1)
 
-    trainfile = sys.argv[1]  #
-    validationfile = sys.argv[2]  #
-    modelname_prefix = sys.argv[3]  # # e.g., $MODELS/bert_cnn
+    trainfile = sys.argv[1]
+    validationfile = sys.argv[2]
+    modelname_prefix = sys.argv[3]
 
-    # load train and validation data
-    traindata = Dataset(trainfile)  #
-    valdata = Dataset(validationfile)  #
+    traindata = Dataset(trainfile)
+    valdata = Dataset(validationfile)
 
-    # create indexes from training data
-    # This max_len is for BERT tokenizer, not words.
-    max_len = 150  # As per your original max_len
+    max_len = 150
 
-    # Initialize Codemaps with BERT tokenizer settings
-    codes = Codemaps(traindata, maxlen=max_len, bert_model_name=BERT_MODEL_NAME)  #
+    # For Codemaps, we can use BertTokenizerFast to be consistent with the example provided by user
+    # Ensure your Codemaps is adapted or that BertTokenizerFast is used there.
+    # For simplicity, I'll assume Codemaps uses a compatible tokenizer or you adapt it.
+    # The crucial part is that the tokenizer used for preparing data MATCHES what BERT expects,
+    # including added special tokens.
 
-    # Resize BERT model's token embeddings if new special tokens were added
-    # This is important if '<DRUG1>', etc. were not in PharmBERT's original vocab
-    temp_bert_model_for_resize = TFBertModel.from_pretrained(BERT_MODEL_NAME)
-    temp_bert_model_for_resize.resize_token_embeddings(len(codes.tokenizer))
+    # Let's use BertTokenizerFast here for resizing, assuming Codemaps will align.
+    tokenizer_for_resize = BertTokenizerFast.from_pretrained(BERT_MODEL_NAME)
+    special_tokens_dict = {
+        "additional_special_tokens": ["<DRUG1>", "<DRUG2>", "<DRUG_OTHER>"]
+    }
+    tokenizer_for_resize.add_special_tokens(special_tokens_dict)
 
-    # build network
-    # Consider a smaller learning rate for fine-tuning BERT, e.g., 2e-5, 3e-5, 5e-5
-    model = build_network(codes, learning_rate=3e-5)  #
-    with redirect_stdout(sys.stderr):  #
-        model.summary()  #
+    print(f"Tokenizer vocabulary size before resizing: {len(tokenizer_for_resize)}")
 
-    # encode datasets
-    # encode_texts returns a dict: {'input_ids': ..., 'attention_mask': ...}
+    # Initialize a temporary BERT model just for resizing its token embeddings
+    # This ensures that the weights loaded by the BertEmbeddingLayer later are compatible.
+    temp_bert_model_for_resize = TFBertModel.from_pretrained(
+        BERT_MODEL_NAME, from_pt=True
+    )
+    temp_bert_model_for_resize.resize_token_embeddings(len(tokenizer_for_resize))
+    print(f"Resized BERT model for tokenizer vocab size: {len(tokenizer_for_resize)}")
+    # We don't use temp_bert_model_for_resize directly in the main model,
+    # the BertEmbeddingLayer will load a fresh one with resized embeddings available.
+    # The resize_token_embeddings call modifies the model's config that from_pretrained uses.
+    # (This step is subtle: resize_token_embeddings on a standalone model instance prepares
+    # the ground for future from_pretrained calls IF the config is saved and reloaded,
+    # or if the same model instance is used. Here, we are ensuring the vocabulary size is known.)
+    # A more robust way to ensure the main model gets resized embeddings if not done globally
+    # by Hugging Face caching/config updates would be to pass the resized model/config
+    # to the custom layer or resize within the custom layer.
+    # However, usually, `resize_token_embeddings` when called once for a tokenizer/model pair
+    # influences subsequent `from_pretrained` calls if the tokenizer length is different.
+    # The BertEmbeddingLayer should pick up the model with correctly sized embeddings
+    # because the tokenizer passed to DataCollator/used in Codemaps has the new tokens.
+
+    # Update Codemaps to use BertTokenizerFast and handle special tokens consistently
+    # This is CRITICAL. The tokenizer in Codemaps MUST be the same instance or configuration
+    # as tokenizer_for_resize (especially vocab and added tokens).
+    codes = Codemaps(
+        traindata,
+        maxlen=max_len,
+        bert_model_name=BERT_MODEL_NAME,
+        tokenizer_class=BertTokenizerFast,
+    )
+    # Ensure your Codemaps __init__ and encode_texts are updated to use the passed tokenizer_class
+    # and add the special tokens like ['<DRUG1>', '<DRUG2>', '<DRUG_OTHER>']
+
+    # Verify vocab size consistency
+    if len(codes.tokenizer.get_vocab()) != len(tokenizer_for_resize.get_vocab()):
+        print(
+            "WARNING: Tokenizer vocab size mismatch between Codemaps and resizing step!"
+        )
+        print(f"Codemaps tokenizer vocab size: {len(codes.tokenizer.get_vocab())}")
+        print(f"Resizing tokenizer vocab size: {len(tokenizer_for_resize.get_vocab())}")
+        # This usually happens if special tokens are not added consistently in both places.
+        # Ensure Codemaps's internal tokenizer also calls `add_special_tokens`.
+
+    model = build_network(codes, learning_rate=3e-5)
+    with redirect_stdout(sys.stderr):
+        model.summary()
+
     Xt_encoded = codes.encode_texts(traindata)
-    Yt = codes.encode_labels(traindata)  #
+    Yt = codes.encode_labels(traindata)
 
     Xv_encoded = codes.encode_texts(valdata)
-    Yv = codes.encode_labels(valdata)  #
+    Yv = codes.encode_labels(valdata)
 
-    # train model
-    with redirect_stdout(sys.stderr):  #
+    with redirect_stdout(sys.stderr):
         model.fit(
             [Xt_encoded["input_ids"], Xt_encoded["attention_mask"]],
             Yt,
-            batch_size=64,  # BERT models are memory intensive, you might need to reduce batch size from 32
-            epochs=3,  # Fine-tuning BERT usually requires fewer epochs (e.g., 3-5)
+            batch_size=16,
+            epochs=5,
             validation_data=(
                 [Xv_encoded["input_ids"], Xv_encoded["attention_mask"]],
                 Yv,
@@ -144,9 +200,13 @@ if __name__ == "__main__":
             verbose=1,
         )
 
-    # save model and codemaps (which now saves maxlen, label_index, bert_model_name)
-    model.save(modelname_prefix + ".keras")  #
-    codes.save(modelname_prefix + ".idx")  #
+    # When saving, Keras will try to save the custom layer.
+    # For loading, you'll need to provide it in custom_objects.
+    model.save(modelname_prefix + ".keras")
+    codes.save(modelname_prefix + ".idx")
 
     print(f"Model saved to {modelname_prefix}.keras")
     print(f"Codemaps saved to {modelname_prefix}.idx")
+    print(
+        "To load the model later, use: \nfrom tensorflow.keras.models import load_model\nmodel = load_model('your_model.keras', custom_objects={'BertEmbeddingLayer': BertEmbeddingLayer})"
+    )
